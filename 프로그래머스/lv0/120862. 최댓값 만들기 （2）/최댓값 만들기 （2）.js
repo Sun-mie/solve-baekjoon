31122d4b7dc9d4408c70e6b61949d6da737a277b
Sun-mie <ssunm2@kakao.com>
2023-04-16 22:54:12 +0900

    Delete 프로그래머스/lv0/120862. 최댓값 만들기 （2） directory

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++
@@ -1,9 +0,0 @@
-function solution(numbers) {
-    let arr = [];
-    for(let i=0; i<numbers.length; i++){
-        for(let v=i+1; v<numbers.length; v++){
-            arr.push(numbers[i] * numbers[v]);
-        }
-    }
-    return arr.sort((a, b)=>b-a)[0];
-}
