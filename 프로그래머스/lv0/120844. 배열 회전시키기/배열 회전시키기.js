af7eef11a399f05b90ba3811dc6c266f1a91043b
Sun-mie <ssunm2@kakao.com>
2023-04-23 18:10:06 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,11 +1,9 @@
 function solution(numbers, direction) {
     let answer = [...numbers]
     if(direction === 'right'){
-        answer.unshift(answer.slice(-1)[0])
-        answer.pop()
+        answer.unshift(answer.pop())
     }else{
-        answer.push(answer[0])
-        answer.splice(0, 1)
+        answer.push(answer.shift())
     }
     return answer;
 }
