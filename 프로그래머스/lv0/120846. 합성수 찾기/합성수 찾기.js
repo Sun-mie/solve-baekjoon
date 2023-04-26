0bb372556df07cf7c8d9a72e202a3b8576b1c0d5
Sun-mie <ssunm2@kakao.com>
2023-04-27 00:10:12 +0900

    [level 0] Title: 합성수 찾기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let arry = Array(n).fill('').filter((val, idx, arr)=>{
+        let cnt = 0;
+        for(let i=0; i<=arr.length; i++){
+            if((idx+1)%i===0) cnt++;
+            if(cnt>=3) return true;
+        }
+    })
+    return arry.length;
+}
