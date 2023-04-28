6a98bc83720831a3e75fca9edffe63788952e106
Sun-mie <ssunm2@kakao.com>
2023-04-28 23:37:44 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,9 +1,3 @@
 function solution(my_string) {
-    let temp_set = new Set();
-    let answer = [];
-    for(let a of my_string){
-        temp_set.add(a);
-    }
-    temp_set.forEach(a => answer.push(a));
-    return answer.join('');
+    return [...new Set(my_string)].join('');
 }
