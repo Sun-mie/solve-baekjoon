8b48fae927ea62db78b56ea3bb180399b6f76412
Sun-mie <ssunm2@kakao.com>
2023-04-30 02:28:11 +0900

    [level 0] Title: A로 B 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,10 +1,11 @@
 function solution(before, after) {
-    let answer = [...before]
-    for(let v of after){
-        let chk = answer.findIndex(a => a == v)
-        if (chk >= 0){
-            answer.splice(chk ,1)
-        }
-    }
-    return answer.length == 0 ? 1 : 0;
+    // let answer = [...before]
+    // for(let v of after){
+    //     let chk = answer.findIndex(a => a == v)
+    //     if (chk >= 0){
+    //         answer.splice(chk ,1)
+    //     }
+    // }
+    // return answer.length == 0 ? 1 : 0;
+    return [...before].sort().join('') === [...after].sort().join('') ? 1: 0 ;
 }
