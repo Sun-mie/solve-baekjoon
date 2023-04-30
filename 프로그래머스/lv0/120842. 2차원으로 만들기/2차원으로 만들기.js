ffb46a21902e55171f1fd9e0a54bfb6ca79deacb
Sun-mie <ssunm2@kakao.com>
2023-04-30 20:28:35 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,7 +1,18 @@
 function solution(num_list, n) {
-    let answer = [];
-    for(let i=0; i<num_list.length; i+=n){
-        answer.push(num_list.slice(i, i+n))
+    // [1]
+    // let answer = [];
+    // for(let i=0; i<num_list.length; i+=n){
+    //     answer.push(num_list.slice(i, i+n))
+    // }
+    // return answer;
+    
+    // [2]
+    // return new Array(num_list.length / n).fill([]).map(()=>num_list.splice(0, n));
+    
+    // [3]
+    let arr = []
+    while(num_list.length){
+        arr.push(num_list.splice(0,n))
     }
-    return answer;
+    return arr;
 }
