e5851a5f6a09521f92222b9e80ab1d237de3651a
Sun-mie <ssunm2@kakao.com>
2023-05-02 00:03:58 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,8 +1,13 @@
 function solution(n) {
-    let answer = 1;
-    let multi = 0;
-    while(answer <= n){
-        answer *= ++multi;
-    }
-    return multi-1;
+    // let answer = 1;
+    // let multi = 0;
+    // while(answer <= n){
+    //     answer *= ++multi;
+    // }
+    // return multi-1;
+    
+    let i = 1;
+    let f = 1;
+    while (f*i < n) f*=++i;
+    return i;
 }
