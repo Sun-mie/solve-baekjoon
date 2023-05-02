d0386155962ef676e50a0ca9969ff33fbe09669d
Sun-mie <ssunm2@kakao.com>
2023-05-02 21:12:47 +0900

    [level 0] Title: k의 개수, Time: 6.28 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,9 +1,15 @@
 function solution(i, j, k) {
-    let cnt = i;
-    let answer = 0;
-    while(cnt<=j){
-        answer += cnt.toString().split('').filter(v=> Number.parseInt(v) === Number.parseInt(k)).length
-        cnt++
+    // let cnt = i;
+    // let answer = 0;
+    // while(cnt<=j){
+    //     answer += cnt.toString().split('').filter(v=> Number.parseInt(v) === Number.parseInt(k)).length
+    //     cnt++
+    // }
+    // return answer;
+    
+    let answer = '';
+    for(i; i<=j; i++){
+        answer += i
     }
-    return answer;
+    return answer.split(k).length-1;
 }
