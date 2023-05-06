61f58ad1026afa7606de3ba0a840a89b67488b1f
Sun-mie <ssunm2@kakao.com>
2023-05-07 02:26:42 +0900

    [level 0] Title: 가까운 수, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,7 +1,7 @@
 function solution(array, n) {
-    console.log(array.sort((a, b)=>{
-        if(Math.abs(a-n) === Math.abs(b-n)) return a-b;
-        else return Math.abs(a-n)-Math.abs(b-n);
-    }))
+    array.sort((a, b)=> Math.abs(a-n) - Math.abs(b-n) || a-b)
     return array[0];
+    
+    // return array.reduce((a, c)=>{
+    //     return Math.abs(a-n) < Math.abs(c-n) ? a : Math.abs(a-n) === Math.abs(c-n) ? Math.min(a, c) : c}) ;
 }
