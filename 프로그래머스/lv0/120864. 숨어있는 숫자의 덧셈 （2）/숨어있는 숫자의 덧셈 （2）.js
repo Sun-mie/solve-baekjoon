7ce3b8d9bef99d5854e14c199386126ffef6e98b
Sun-mie <ssunm2@kakao.com>
2023-05-08 23:47:07 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 2.56 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,3 +1,14 @@
 function solution(my_string) {
-    return my_string.split(/[a-zA-Z]/).filter(v=>v!='').reduce((a, c)=> a+=Number.parseInt(c), 0);
+    
+    // parseInt()와 Number()의 차이
+    console.log(parseInt('')) // NaN
+    console.log(Number('')) // 0
+    console.log(parseInt('2023년')) // 2023
+    console.log(Number('2023년')) // NaN
+    console.log(parseInt('제1회')) // NaN
+    console.log(Number('제1회')) // NaN
+    console.log(parseInt('10.1234')) // 10
+    console.log(Number('10.1234')) // 10.1234
+    
+    return my_string.split(/[a-zA-Z]/).reduce((a, c)=> a+=Number(c), 0);
 }
