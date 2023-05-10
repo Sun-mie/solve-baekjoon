509ef4762f9ee418180805aea9cbe01e1b1c927c
Sun-mie <ssunm2@kakao.com>
2023-05-11 00:18:52 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,7 +1,11 @@
 function solution(s) {
-    let temp = {}
+    // let temp = {}
+    // let answer = [];
+    // [...s].map(v => temp[v]>0 ? temp[v] += 1 : temp[v] = 1);
+    // Object.keys(temp).map(v => {if(temp[v] === 1) answer.push(v)});
+    // return answer.sort().join('');
+    
     let answer = [];
-    [...s].map(v => temp[v]>0 ? temp[v] += 1 : temp[v] = 1);
-    Object.keys(temp).map(v => {if(temp[v] === 1) answer.push(v)});
+    for(let v of s) if(s.indexOf(v) === s.lastIndexOf(v)) answer.push(v);
     return answer.sort().join('');
 }
