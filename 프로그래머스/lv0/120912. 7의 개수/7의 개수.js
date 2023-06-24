170000925a11e46e0cd541ce37dfbe38aa0b0875
Sun-mie <ssunm2@kakao.com>
2023-06-25 01:50:07 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 32.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,3 +1,4 @@
 function solution(array) {
-    return array.join('').split('').filter(v=>/7+/.test(v)).length;
+    //return array.join('').split('').filter(v=>/7+/.test(v)).length;
+    return array.join('').split('7').length-1;
 }
